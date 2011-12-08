d37dd8693e81d13b997536dc805a2daa10740e6e
higgs.ml <higgs.ml@3d821594-119b-d7cc-d0cd-aab8c6a4165b>
2011-12-08 14:28:34 +0000

    match input names

--- src/util/WrapperTool.java
+++ src/util/WrapperTool.java
@@ -1,24 +1,30 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WrapperTool {
 
 	private static void printUsageAndExit() {
 		System.err
-				.println("Usage: java util.WrapperTool <beast1 class> "
+				.println("Usage: java util.WrapperTool <beast1 class> <beast1 src path>\n"
 						+ "Creates a wrapper class that allows using beast1 classes in beast2\n"
 						+ "<beast1 class> name of the class to create a wrapper for\n"
+						+ "<beast1 src path> path of the source class\n"
+						+ "Example: java util.WrapperTool dr.evomodel.clock.UniversalClock ..\beast-mcmc\n"
 						+ "Note that beast1 must be in the class path!");
 		System.exit(0);
 	}
 
-	private static void processClass(String className) {
+	private static void processClass(String className, String srcPath) {
 		String shortClass = className.substring(className.lastIndexOf('.') + 1);
 		Set<String> constructorArgs = new HashSet<String>();
 		try {
@@ -39,13 +45,15 @@ public class WrapperTool {
 			Set<String> imports = new HashSet<String>();
 			imports.add(className);
 			imports.add("beast.core.*");
-			
+			String[] inputNames = guessInputNames(srcPath, className,
+					pType.length);
+
 			for (int i = 0; i < pType.length; i++) {
 				Type[] gpType = constructorWithMostArguments
 						.getGenericParameterTypes();
 
 				for (int j = 0; j < gpType.length; j++) {
-					String type  = gpType[j].toString();
+					String type = gpType[j].toString();
 					if (type.contains(" ")) {
 						type = type.split(" ")[1];
 					}
@@ -61,28 +69,33 @@ public class WrapperTool {
 					if (type.contains(".")) {
 						if (type.contains("<")) {
 							imports.add(type.substring(0, type.indexOf('<')));
-							type = type.substring(type.lastIndexOf('.', type.indexOf('<')) + 1);
+							type = type.substring(type.lastIndexOf('.', type
+									.indexOf('<')) + 1);
 						} else {
-							imports.add(type);							
+							imports.add(type);
 							type = type.substring(type.lastIndexOf('.') + 1);
 						}
 
 					}
-					inputs += "    new Input<" + type + "> input" + j + 
-							" = new Input<" + type + ">(\"input" + j + "\", \"description here\");\n";
+					inputs += "    new Input<" + type + "> " + inputNames[j]
+							+ " = new Input<" + type + ">(\"" + inputNames[j]
+							+ "\", \"description here\");\n";
 				}
 				break;
 			}
 
-			System.out.println("package beast." + className.substring(3) + ";\n\n");
+			System.out.println("package beast." + className.substring(3)
+					+ ";\n\n");
 
-			String [] s = imports.toArray(new String[0]);
+			String[] s = imports.toArray(new String[0]);
 			Arrays.sort(s);
 			for (String importName : s) {
 				System.out.println("import " + importName + ";");
 			}
 			System.out.println("\n\n@Description(\"...\")");
-			System.out.println("class " + className.substring(className.lastIndexOf('.') + 1) + " extends Plugin {");
+			System.out.println("class "
+					+ className.substring(className.lastIndexOf('.') + 1)
+					+ " extends Plugin {");
 			System.out.println(inputs);
 			System.out.println("\n");
 			String objectName = shortClass.toLowerCase();
@@ -90,10 +103,13 @@ public class WrapperTool {
 			System.out.println("\n");
 
 			System.out.println("    @Override");
-			System.out.println("    public void initAndValidate() throws Exception {");
-			System.out.println("        " + objectName + " = new " + shortClass +"(");
+			System.out
+					.println("    public void initAndValidate() throws Exception {");
+			System.out.println("        " + objectName + " = new " + shortClass
+					+ "(");
 			for (int j = 0; j < pType.length; j++) {
-				System.out.print("                             input" + j + ".get()");
+				System.out.print("                             " + inputNames[j]
+						+ ".get()");
 				if (j < pType.length - 1) {
 					System.out.println(",");
 				}
@@ -101,7 +117,7 @@ public class WrapperTool {
 			System.out.println(");");
 			System.out.println("    }");
 			System.out.println("\n");
-			
+
 			System.out.println("}");
 		} catch (ClassNotFoundException x) {
 			throw new RuntimeException("Could not find class " + className
@@ -110,12 +126,71 @@ public class WrapperTool {
 
 	}
 
+	private static String[] guessInputNames(String srcPath, String className,
+			int length) {
+		String sFileName = srcPath + "/" + className.replaceAll("\\.", "/")
+				+ ".java";
+		try {
+			// grab file text
+			BufferedReader fin = new BufferedReader(new FileReader(sFileName));
+			StringBuffer buf = new StringBuffer();
+			String sStr = null;
+			while (fin.ready()) {
+				sStr = fin.readLine();
+				buf.append(sStr);
+				buf.append('\n');
+			}
+			fin.close();
+			String text = buf.toString();
+			
+			// remove comments
+			text = text.replaceAll("//[^\n]*\n", "");
+			text = text.replaceAll(
+					"(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)", "");
+			String shortClass = className.substring(1+className.lastIndexOf('.'));
+			text = text.replaceAll(".*" + shortClass, shortClass);
+			
+			// build regexp
+			String[] inputNames = new String[length];
+			String regexp = ".*" + className.substring(1+className.lastIndexOf('.')) + "\\s*\\(";
+			for (int i = 0; i < length; i++) {
+				regexp += "([^,\\)]*)";
+				if (i < length - 1) {
+					regexp += ",";
+				}
+			}
+			regexp += ".*\\).*";
+			//System.err.println(regexp);
+			
+			// match regexp
+			Pattern pattern = Pattern.compile(regexp, Pattern.MULTILINE);
+			Matcher matcher = pattern.matcher(text);
+			matcher.find();
+			for (int i = 0; i < length; i++) {
+				String param = matcher.group(i + 1);
+				param = param.replaceAll("^\\s+", "");
+				param = param.replaceAll("\\s+$", "");
+				param = param.split(" ")[1];
+				inputNames[i] = param;
+			}
+			return inputNames;
+		} catch (Exception e) {
+			System.err.println("Warning: " + e.getMessage());
+			String[] inputNames = new String[length];
+			for (int i = 0; i < length; i++) {
+				inputNames[i] = "input" + i;
+			}
+			return inputNames;
+		}
+	}
+
 	public static void main(String[] args) {
-		if (args.length != 1) {
+		if (args.length != 2) {
 			printUsageAndExit();
 		}
 		String className = args[0];
-		processClass(className);
+		String srcPath = args[1];
+		processClass(className, srcPath);
 	}
 
 }
