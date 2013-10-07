9300e84c759ff2bbe0b246d1f0bdc8db2bd8a48d
higgs.ml <higgs.ml@3d821594-119b-d7cc-d0cd-aab8c6a4165b>
2013-10-07 00:48:03 +0000

    missing file added

---
+++ trunk/src/beast/evolution/likelihood/LeafTrait.java
@@ -0,0 +1,17 @@
+package beast.evolution.likelihood;
+
+import beast.core.CalculationNode;
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Input.Validate;
+import beast.core.parameter.IntegerParameter;
+
+@Description("For associating a taxon with its parameter")
+public class LeafTrait extends CalculationNode {
+	public Input<String> taxonName = new Input<String>("taxon", "taxon name identifying the leaf", Validate.REQUIRED);
+	public Input<IntegerParameter> parameter = new Input<IntegerParameter>("parameter", "parameter associated with the leaf", Validate.REQUIRED);
+	
+	public void initAndValidate() throws Exception {
+		// nothing to do
+	};
+}
